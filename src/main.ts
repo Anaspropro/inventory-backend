b0553250df79bda02dc3f6524e708f69c1f13d6a
Webwiz <aliuanas004@gmail.com>
2025-08-08 08:22:12 +0100

    listen frontend

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
+  await ConfigModule.forRoot({
+    isGlobal: true,
+  });
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
@@ -13,10 +17,16 @@ async function bootstrap() {
       skipMissingProperties: true, // Skip validation for missing properties
     }),
   );
+  const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    'http://localhost:3000',
+  ].filter(Boolean); // removes undefined
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: allowedOrigins,
     credentials: true,
   });
+
   const port = process.env.PORT ? Number(process.env.PORT) : 3001;
   await app.listen(port);
   console.log(`Server running on port ${port}`);
