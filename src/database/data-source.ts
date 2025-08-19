628849c3a3507152484bc398c79acbf62be70d3a
Webwiz <aliuanas004@gmail.com>
2025-08-19 18:52:32 +0100

    finishing...

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,15 +1,5 @@
-// backend/src/database/data-source.ts
 import { DataSource } from 'typeorm';
 import { Product } from '../products/entities/product.entity';
-// import { User } from '../users/entities/user.entity';
-// import { Category } from '../categories/entities/category.entity';
-// import { Supplier } from '../suppliers/entities/supplier.entity';
-// import { Order } from '../orders/entities/order.entity';
-// import { OrderItem } from '../orders/entities/order-item.entity';
-// import { Sale } from '../sales/entities/sale.entity';
-// import { SaleItem } from '../sales/entities/sale-item.entity';
-// import { InventoryMovement } from '../inventory/entities/inventory-movement.entity';
-// import { Report } from '../reports/entities/report.entity';
 import * as dotenv from 'dotenv';
 import { Category } from 'src/categories/entities/category.entity';
 import { Sale } from 'src/sales/entities/sale.entity';
@@ -19,19 +9,8 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: [
-    Product,
-    // User,
-    Category,
-    // Supplier,
-    // Order,
-    // OrderItem,
-    Sale,
-    // SaleItem,
-    // InventoryMovement,
-    // Report,
-  ],
-  synchronize: true, // Don't use in production
+  entities: [Product, Category, Sale],
+  synchronize: false, // Don't use in production
   logging: true,
   ssl: {
     rejectUnauthorized: false,
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,12 +4,6 @@ import { ConfigModule } from '@nestjs/config';
 import { Product } from '../products/entities/product.entity';
 import { Sale } from '../sales/entities/sale.entity';
 import { Category } from '../categories/entities/category.entity';
-// import { User } from '../users/entities/user.entity';
-// import { Supplier } from '../suppliers/entities/supplier.entity';
-// import { Order } from '../orders/entities/order.entity';
-// import { OrderItem } from '../orders/entities/order-item.entity';
-// import { InventoryMovement } from '../inventory/entities/inventory-movement.entity';
-// import { Report } from '../reports/entities/report.entity';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -19,18 +13,8 @@ dotenv.config();
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      entities: [
-        Product,
-        Sale,
-        Category,
-        // User,
-        // Supplier,
-        // Order,
-        // OrderItem,
-        // InventoryMovement,
-        // Report,
-      ],
-      synchronize: true,
+      entities: [Product, Sale, Category],
+      synchronize: false,
       ssl: {
         rejectUnauthorized: false,
       },
